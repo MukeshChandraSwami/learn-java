90a121b3a243c3855086111f7ce508f7e5852444
mukesh swami <mukesh.swami.501598@gmail.com>
2022-03-22 09:16:49 +0530

    Java 14 learnings

--- learn-java/learn-java14/src/main/java/java14/records/StudentRecord.java
+++ learn-java/learn-java14/src/main/java/java14/records/StudentRecord.java
@@ -23,7 +23,7 @@ public record StudentRecord(String name, int rollNum) { // Parametrised construc
     }
 
     public static void main(String[] args) {
-        StudentRecord st = new StudentRecord(null, 101);
+        StudentRecord st = new StudentRecord("Rahul", 101);
         System.out.println(st); // toString() method call, automatically provided by JVM.
         System.out.println(st.name()); // Getter method call, automatically provided by JVM.
         System.out.println(st.getSchool()); // Instance method call.
